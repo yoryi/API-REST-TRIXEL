e13fe7238b4d851ada3ae4882fcf7c2ec86246b0
YORYI OJEDA <yoryiojeda@outlook.com>
2020-01-15 16:47:45 -0500

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tasks.tasks.wsgi:app --workers 16
---
+++ tasks/tasks/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+
+from rest_framework.routers import DefaultRouter
+from Peliculas.views import Peliculas_Populares_ViewSet
+
+
+router = DefaultRouter()
+router.register(r'', Peliculas_Populares_ViewSet)
+
+urlpatterns = router.urls 
+
+urlpatterns  += [
+    path('admin/', admin.site.urls),
+]
